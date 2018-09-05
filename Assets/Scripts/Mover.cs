a5606088dfd1594a90cbfb8030d10008797f7a69
Fernando Garcia <fernando.garcia@etermax.com>
2018-09-05 12:10:46 -0300

    NAV MESH: movilidad a un personaje mediante Scripts: NavMesh(seteo superficies caminables y no caminables; Agrego personaje (cilindro) y le doy comportamiento mediante script mover)

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	private Vector3 goal;
+	private UnityEngine.AI.NavMeshAgent agent;
+	//public GameObject cameraParent;
+
+	void Start () {
+		Cursor.visible = false;
+
+		goal = transform.position;
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+	}
+
+	void Update () {
+		goal = transform.position
+                        + Vector3.right * Input.GetAxis("Horizontal")
+                        + Vector3.forward * Input.GetAxis("Vertical");
+  //      + cameraParent.transform.right * Input.GetAxis ("Horizontal") 
+		//+ cameraParent.transform.forward * Input.GetAxis ("Vertical");
+		
+		agent.destination = goal;
+	}
+}
