c3c127210e5020a6f27ab50ef159da84451db972
Fernando Garcia <fernando.garcia@etermax.com>
2018-09-09 19:28:14 -0300

    Enemies drop loot: Los enemigos arrojan monedas, animo monedas

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -10,7 +10,9 @@ public class Projectile : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Enemy") {
 			Destroy (other.gameObject);
-			Destroy (gameObject);
+            Instantiate(Resources.Load("Collectable"), other.transform.position, Quaternion.Euler(-90,0,0));
+            Instantiate(Resources.Load("PickupEffect"), other.transform.position, Quaternion.Euler(-90, 0, 0));
+            Destroy (gameObject);
 		}
 	}
 }
