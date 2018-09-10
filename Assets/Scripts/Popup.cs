69b2933b2dc97a2c92fa679187ab67ae46359818
Fernando Garcia <fernando.garcia@etermax.com>
2018-09-10 17:56:09 -0300

    Reach the end of the level: Muestro al jugador si gano o perdio

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Popup : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Enemy") {
+			SceneManager.LoadScene (0);
+		}
+		if (other.tag == "Goal") {
+			SceneManager.LoadScene (0);
+		}
+	}
+}
